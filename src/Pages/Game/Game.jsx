69989adeee02cd7b5937324cdc60a14f1f1172b1
Anna Klimagina <116626383+AnnKlii@users.noreply.github.com>
2024-06-27 11:10:38 +0300

    Add files via upload

--- src/Pages/Game/Game.jsx
+++ src/Pages/Game/Game.jsx
@@ -1,11 +1,11 @@
 import styles from './Game.module.css';
-import Word from '../../components/Word/Word';
+import Word from '../../Components/Word/Word';
 import { BsArrowLeftCircle, BsArrowRightCircle } from "react-icons/bs";
 import { useState } from 'react';
 
 
 export default function Game({ words }) {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
     function nextSlide() {
         if (count === words.length - 1) {
@@ -22,15 +22,18 @@ export default function Game({ words }) {
         }
         setCount(count - 1)
     }
-    return (
-        <div className={styles.game}>
-            <BsArrowLeftCircle className={styles.slider} onClick={prevSlide} />
-            <div>
-                <Word {...words[count]} />
-                <p className={styles.count}>Слово {count + 1} из {words.length}</p>
-            </div>
 
-            <BsArrowRightCircle className={styles.slider} onClick={nextSlide} />
-        </div >
+    return (
+        <>
+            <div className={styles.game}>
+                <BsArrowLeftCircle className={styles.slider} onClick={prevSlide} />
+                <div>
+                    <Word {...words[count]} key={words[count].id} />
+                    <p className={styles.count}>Слово {count + 1} из {words.length}</p>
+                </div>
+
+                <BsArrowRightCircle className={styles.slider} onClick={nextSlide} />
+            </div >
+        </>
     )
 }
